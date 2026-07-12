b9e1cf2998491ea82fc91b6dbddf2e7853e4d3a1
Alex Van Maele <alexander.vanmaele@vlaanderen.be>
2026-07-12 17:34:42 +0200

    feat: add custom favicon and configure Nuxt app head

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,4 +7,12 @@ export default defineNuxtConfig({
   },
 
   css: ['~/assets/css/main.css'],
+
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+      ],
+    },
+  },
 })
